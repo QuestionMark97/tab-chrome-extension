6876a3249007a6ec1827b64c2149a2fd4ece8f4e
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-03-05 13:29:51 -0500

    Change input to account for hours, minutes, and seconds

--- popup.js
+++ popup.js
@@ -3,14 +3,17 @@ const declutter = document.getElementById('declutter');
 const fiveSec = document.getElementById('5-sec-btn');
 const fiveMin = document.getElementById('5-min-btn');
 
-const time = document.getElementById('time-input');
+const hr = document.getElementById('hr-input');
+const min = document.getElementById('min-input');
+const sec = document.getElementById('sec-input');
 const timeButton = document.getElementById('time-input-button');
 
 const timeArr = [];
 
 // Timed declutter
 timeButton.addEventListener('click', () => {
-  timeArr.push(time.value);
+  const time = ((((hr.value * 60) + min.value) * 60) + sec.value) * 1000;
+  timeArr.push(time);
   chrome.runtime.sendMessage({ time: timeArr[0] });
 });
 
@@ -24,7 +27,7 @@ fiveMin.addEventListener('click', () => {
 
 // Instant declutter
 declutter.addEventListener('click', () => {
-  chrome.tabs.query({ currentWindow: true }, tabs => {
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
     for (let i = 0; i < tabs.length; i++) {
       if (tabs[i].active === false) {
         chrome.tabs.remove(tabs[i].id);
