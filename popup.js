182c1d3167d2173533cd95bb04e56dd89f50e915
Zac H <zac.haluza@gmail.com>
2020-03-04 23:54:12 -0500

    added declutter now feature

--- popup.js
+++ popup.js
@@ -1,5 +1,16 @@
 const btn = document.querySelector('.btn');
+const declutter = document.getElementById('declutter');
 
 btn.addEventListener('click', () => {
   chrome.runtime.sendMessage({ whitelist: [] });
+});
+
+declutter.addEventListener('click', () => {
+  chrome.tabs.query({ currentWindow: true }, tabs => {
+    for (let i = 0; i < tabs.length; i++) {
+      if (tabs[i].active === false) {
+        chrome.tabs.remove(tabs[i].id);
+      }
+    }
+  });
 });
