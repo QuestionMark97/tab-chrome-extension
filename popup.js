f356f0cb122c5c00ed3e774089753da6d84b4a4d
Zac H <zac.haluza@gmail.com>
2020-03-05 13:45:02 -0500

    styling changes

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById('btn');
 const declutter = document.getElementById('declutter');
-const fiveSec = document.getElementById('5-sec-btn');
 const fiveMin = document.getElementById('5-min-btn');
 
 const hr = document.getElementById('hr-input');
@@ -12,22 +11,18 @@ const timeArr = [];
 
 // Timed declutter
 timeButton.addEventListener('click', () => {
-  const time = ((((hr.value * 60) + min.value) * 60) + sec.value) * 1000;
+  const time = ((hr.value * 60 + min.value) * 60 + sec.value) * 1000;
   timeArr.push(time);
   chrome.runtime.sendMessage({ time: timeArr[0] });
 });
 
-fiveSec.addEventListener('click', () => {
-  chrome.runtime.sendMessage({ time: 1000 });
-});
-
 fiveMin.addEventListener('click', () => {
-  chrome.runtime.sendMessage({ time: 5000 });
+  chrome.runtime.sendMessage({ time: 1000 });
 });
 
 // Instant declutter
 declutter.addEventListener('click', () => {
-  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+  chrome.tabs.query({ currentWindow: true }, tabs => {
     for (let i = 0; i < tabs.length; i++) {
       if (tabs[i].active === false) {
         chrome.tabs.remove(tabs[i].id);
