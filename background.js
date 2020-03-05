11491a84b512c44cbd4340a28be5f73fba22ed96
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-03-04 23:05:35 -0500

    Finished basic functionality utilizing 'Message' api

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
-// chrome.commands.onCommand.addListener(command => {
-//   if (command === 'close-tab') {
-//     chrome.tabs.query({ currentWindow: true }, tabs => {
-//       for (let i = 0; i < tabs.length; i++) {
-//         if (tabs[i].active === false) {
-//           setTimeout(() => {
-//             chrome.tabs.remove(tabs[i].id);
-//           }, 1000);
-//         }
-//       }
-//     });
-//   }
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ whitelist: [] });
 // });
+chrome.runtime.onMessage.addListener(
+  () => {
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].active === false) {
+          setTimeout(() => {
+            chrome.tabs.remove(tabs[i].id);
+          }, 1000);
+        }
+      }
+    });
+  },
+);
--- popup.js
+++ popup.js
@@ -1,13 +1,5 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
-  chrome.tabs.query({ currentWindow: true }, tabs => {
-    for (let i = 0; i < tabs.length; i++) {
-      if (tabs[i].active === false) {
-        setTimeout(() => {
-          chrome.tabs.remove(tabs[i].id);
-        }, 1000);
-      }
-    }
-  });
+  chrome.runtime.sendMessage({ whitelist: [] });
 });
